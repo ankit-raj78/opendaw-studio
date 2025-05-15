5ca1fabaec8c506aebc5428c28c5884c954e614f
André Michelle <andre.michelle@gmail.com>
2025-05-15 13:35:37 +0200

    use bash in lib adds set -euo pipefail

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+set -euo pipefail
 export TERM=${TERM:-dumb}
-clear
-set -e
+if [[ -t 1 ]]; then clear; fi
+
 ./clean.sh || exit 1
 (cd lib && ./rebuild.sh) || exit 1
+
 echo "install boxes"
-(cd studio-boxes && npm install) || exit 1
+(cd studio-boxes && npm install)   || exit 1
+
 echo "install studio"
-(cd studio && npm install) || exit 1
+(cd studio && npm install)         || exit 1
+
 echo "gen boxes"
-(cd studio-boxes && npm run gen) || exit 1
+(cd studio-boxes && npm run gen)   || exit 1
+
 echo "build studio"
-(cd studio && npm run build) || exit 1
+(cd studio && npm run build)       || exit 1
+
 echo "done"
