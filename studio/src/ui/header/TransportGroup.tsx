4e71228e47d21434af83e52a08544e9b9eedf460
André Michelle <andre.michelle@gmail.com>
2025-05-30 06:58:19 +0200

    bringing in branch recording

--- studio/src/ui/header/TransportGroup.tsx
+++ studio/src/ui/header/TransportGroup.tsx
@@ -4,7 +4,7 @@ import {createElement} from "jsx"
 import {StudioService} from "@/service/StudioService"
 import {Button} from "@/ui/components/Button.tsx"
 import {Colors} from "@/ui/Colors.ts"
-import {Lifecycle} from "std"
+import {Lifecycle, MutableObservableValueFalse} from "std"
 import {IconSymbol} from "@/IconSymbol.ts"
 
 type Construct = {
@@ -15,7 +15,7 @@ type Construct = {
 export const TransportGroup = ({lifecycle, service: {engine, transport}}: Construct) => (
     <div style={{display: "flex"}}>
         <Checkbox lifecycle={lifecycle}
-                  model={engine.isRecording()}
+                  model={MutableObservableValueFalse}
                   appearance={{activeColor: "hsl(0, 50%, 50%)", tooltip: "Recording"}}>
             <Icon symbol={IconSymbol.Record}/>
         </Checkbox>
