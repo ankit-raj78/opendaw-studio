8a5f8aab4c5f4e7389d4216e344a50eddbbec1f8
André Michelle <andre.michelle@gmail.com>
2025-06-02 15:43:12 +0200

    adds large PianoRoll

--- studio/src/ui/pages/MidiFallPage.tsx
+++ studio/src/ui/pages/MidiFallPage.tsx
@@ -2,13 +2,14 @@ import css from "./MidiFallPage.sass?inline"
 import {createElement, PageContext, PageFactory} from "jsx"
 import {StudioService} from "@/service/StudioService.ts"
 import {Html} from "dom"
+import {PianoRoll} from "@/ui/components/PianoRoll.tsx"
 
 const className = Html.adoptStyleSheet(css, "MidiFallPage")
 
 export const MidiFallPage: PageFactory<StudioService> = ({service}: PageContext<StudioService>) => {
     return (
         <div className={className}>
-            <h1>MidiFallPage</h1>
+            <PianoRoll/>
         </div>
     )
 }
